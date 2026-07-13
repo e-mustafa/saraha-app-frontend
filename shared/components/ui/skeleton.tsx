4fe0936dba20ad90e639cd31c1d255cccaf5413c
e-mustafa <eng.mustafax@gmail.com>
2026-07-14 02:48:30 +0300

    change Skeleton color

--- shared/components/ui/skeleton.tsx
+++ shared/components/ui/skeleton.tsx
@@ -1,7 +1,7 @@
 import { cn } from '@/shared/utils/utils';
 
 function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
-	return <div data-slot='skeleton' className={cn('animate-pulse rounded-md bg-card/40', className)} {...props} />;
+	return <div data-slot='skeleton' className={cn('animate-pulse rounded-md bg-gray-800/50', className)} {...props} />;
 }
 
 export { Skeleton };
